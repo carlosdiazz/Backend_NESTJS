c1df976a941d45d98616148047ba2497dba60547
carlosdiazz <c.diazadriann@gmail.com>
2022-10-26 14:10:46 -0400

    Metodos Get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,37 +10,14 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('nuevo')
-  newEndpoint() {
-    return 'AA';
-  }
+  //@Get('products')
+  //getProducts(@Query() params: any) {
+  //  const { limit, offset } = params;
+  //  return `Productos Limit => ${limit} Offest => ${offset}`;
+  //}
 
-  @Get('products')
-  getProducts(@Query() params: any) {
-    const { limit, offset } = params;
-    return `Productos Limit => ${limit} Offest => ${offset}`;
-  }
-
-  @Get('categories')
-  getCategories(@Query('limit') limit = 100, @Query('offset') offset = 0) {
-    return `Productos Limit => ${limit} Offest => ${offset}`;
-  }
-
-  @Get('product/:id')
-  getProduct(@Param() params: any) {
-    return `Productos ${params.id}`;
-  }
-
-  @Get('tareas/:idTask')
-  getTareas(@Param('idTask') idTask: string) {
-    return `Tarea => ${idTask}`;
-  }
-
-  @Get('categorie/:idCategories/tareas/:idTask')
-  getCategorie(
-    @Param('idTask') idTask: string,
-    @Param('idCategories') idCategories: string,
-  ) {
-    return `Categorie => ${idCategories} Tarea => ${idTask}`;
-  }
+  //@Get('product/:id')
+  //getProduct(@Param() params: any) {
+  //  return `Productos ${params.id}`;
+  //}
 }
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getDefault(@Query('limit') limit = 100, @Query('offset') offset = 0) {
+    return `Esta en la pagina de productos el Offeset es ${offset} y el limit es ${limit}`;
+  }
+
+  @Get(':idProduct')
+  getProduct(@Param('idProduct') idProduct: string) {
+    return `Este es el id => ${idProduct}`;
+  }
+}
