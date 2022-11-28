1fcb408aa2d3dd4e27573f45a42b411e2156337c
Carlos <c.diazadriann@gmail.com>
2022-11-27 20:25:34 -0400

    TQuede a mitad video 12 minuit 14

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -3,7 +3,8 @@ import { SanitizeMongooseModelInterceptor } from 'nestjs-mongoose-exclude';
 //import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginAuth } from '../dto/auth.dto';
-
+import {AuthService } from '../services/auth.service';
+import { User} from '../../components/nosql/users/user.entity';
 @Controller('auth')
 @UseInterceptors(
     new SanitizeMongooseModelInterceptor({
@@ -13,12 +14,17 @@ import { LoginAuth } from '../dto/auth.dto';
   )
 export class AuthController {
 
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('/login')
   @HttpCode(200)
-    //login(@Req() req: Request) {
-    login(@Body() login: LoginAuth) {
+  login(@Req() req: Request) {
+    const user = req.user as User
+    return this.authService.generateJWT(login)
+  }
+  
+      //login(@Body() login: LoginAuth,) {
         //return login.user;
-        return {message:"Logueado"}
-    }
+    //return { message: "Logueado" }
 }
